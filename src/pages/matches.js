bbf153e4f8298a1302f6844a75dcdcd3c3cba0cc
Sahas <sahasvivek001@gmail.com>
2023-02-26 17:55:33 +0530

    feat: matches linked to backend

--- src/pages/matches.js
+++ src/pages/matches.js
@@ -2,18 +2,35 @@ import React from 'react'
 import TopNav from '../components/TopNav'
 import MatchCard from '../components/MatchCard'
 
-const matches = () => {
+const matches = ({
+    data
+}) => {
+    console.log(data[0])
     return (
         <>
             <TopNav />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-10 text-custom-white">
-                <MatchCard />
-                <MatchCard />
-                <MatchCard />
-                <MatchCard />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-10 text-custom-white mx-10">
+                {
+                    data.map((item) => {
+                        return (
+                            <MatchCard item={item} />
+                        )
+                    })
+                }
             </div>
         </>
     )
 }
 
 export default matches
+
+
+export async function getStaticProps() {
+    const res = await fetch("https://iccbackend.up.railway.app/matchs/").then((res) => res.json());
+    console.log(res)
+    return {
+        props: {
+            data: res,
+        },
+    };
+}
---
+++ src/pages/store.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import TopNav from '../components/TopNav'
+import Image from 'next/image'
+import StoreCard from '../components/StoreCard'
+// import MatchCard from '../components/MatchCard'
+
+const store = () => {
+    return (
+        <>
+            <TopNav />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-10 mx-10">
+                <StoreCard />
+                <StoreCard />
+                <StoreCard />
+            </div>
+        </>
+    )
+}
+
+export default store
